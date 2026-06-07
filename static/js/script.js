8d64a4216478585448d23239901c6317f03cbae3
YimeiMaster <111745873+YimeiMaster@users.noreply.github.com>
2026-06-07 14:45:17 +0300

    Delete static directory

--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-// 设置导航栏的活动状态
-document.querySelectorAll('.nav-link').forEach(function(link) {
-    if (window.location.href.indexOf(link.href) > -1) {
-        link.classList.add('active');
-    }
-});
